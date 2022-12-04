b0c1dd9d2bc43fdc0d77485f691584f81be5fc9b
Alejandro Cambronero <cambro97@gmail.com>
2022-12-04 15:06:43 +0100

    1 - Class

--- src/main/java/net/cambrete/modtest/ModTestMod.java
+++ src/main/java/net/cambrete/modtest/ModTestMod.java
@@ -18,13 +18,14 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod(ModTestMod.MOD_ID)
+public class ModTestMod
 {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "modtest";
 
-    public ExampleMod() {
+    public ModTestMod() {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
